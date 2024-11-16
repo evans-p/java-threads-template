d63f093adf34fdd530768d6be21fd104fa0af9c3
evans-p <evanspoulaki@gmail.com>
2024-11-16 23:45:48 +0200

    refactor(RunnablePattern): code refactor.

--- src/main/java/gr/evansp/App.java
+++ src/main/java/gr/evansp/App.java
@@ -6,17 +6,7 @@ package gr.evansp;
  */
 public class App {
     public static void main( String[] args ) throws InterruptedException {
-        doNotDoThis();
+        RunnablePattern.runnableUsage();
     }
 
-    public static void doNotDoThis() throws InterruptedException {
-        Runnable task = () -> {
-            System.out.println("HelloWorld");
-        };
-
-        Thread thread = new Thread(task);
-        thread.run();
-
-        thread.join();
-    }
 }
---
+++ src/main/java/gr/evansp/RunnablePattern.java
@@ -0,0 +1,21 @@
+package gr.evansp;
+
+/**
+ *
+ */
+public class RunnablePattern {
+  private RunnablePattern() {
+    //EMPTY
+  }
+
+  public static void runnableUsage() throws InterruptedException {
+    Runnable task = () -> {
+      System.out.println("HelloWorld");
+    };
+
+    Thread thread = new Thread(task);
+    thread.start();
+
+    thread.join();
+  }
+}
