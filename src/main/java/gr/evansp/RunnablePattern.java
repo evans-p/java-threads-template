b0d7bc61fec81a86c2f02ce43fb9408b94c747de
evans-p <evanspoulaki@gmail.com>
2024-11-16 23:46:09 +0200

    refactor(RunnablePattern): code refactor.

--- src/main/java/gr/evansp/RunnablePattern.java
+++ src/main/java/gr/evansp/RunnablePattern.java
@@ -9,9 +9,7 @@ public class RunnablePattern {
   }
 
   public static void runnableUsage() throws InterruptedException {
-    Runnable task = () -> {
-      System.out.println("HelloWorld");
-    };
+    Runnable task = () -> System.out.println("HelloWorld");
 
     Thread thread = new Thread(task);
     thread.start();
